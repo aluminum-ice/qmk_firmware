a1d1978b90302f0ed23fa404f535f51081872d7f
Elvis <edieguez@ieee.org>
2023-10-01 11:28:47 -0700

    Added new keymap for sofle

---
+++ keyboards/sofle/keymaps/elvis/rules.mk
@@ -0,0 +1,18 @@
+MOUSEKEY_ENABLE = yes
+EXTRAKEY_ENABLE = yes
+RGBLIGHT_ENABLE = yes
+ENCODER_ENABLE = yes
+LTO_ENABLE = yes
+OLED_ENABLE = yes
+OLED_DRIVER = ssd1306
+
+COMBO_ENABLE = yes
+# WPM_ENABLE = yes
+
+SRC += features/casemodes.c
+
+COMMAND_ENABLE   = no  # Commands for debug and configuration
+UNICODE_ENABLE   = no  # Unicode
+SWAP_HANDS_ENABLE= no  # Allow swapping hands of keyboard
+CONSOLE_ENABLE   = no
+MIDI_ENABLE      = no
