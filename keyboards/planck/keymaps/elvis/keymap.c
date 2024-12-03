7bf7d4c692a01b47517c80e582af8ee91b0ae5a2
aluminum-ice <you@example.com>
2024-12-03 13:01:56 -0800

    updated my keymaps

--- keyboards/planck/keymaps/elvis/keymap.c
+++ keyboards/planck/keymaps/elvis/keymap.c
@@ -137,38 +137,39 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_LCTL,  KC_LALT, KC_LGUI, SQL,     LOWER,   SPACEFN, SPACEFN, RAISE,   KC_LEFT, KC_DOWN, KC_UP,     KC_RGHT
 ),
 
-/* Raise
+/* Lower
  * ,-----------------------------------------------------------------------------------.
  * |   ~  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  | Bksp |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   _  |   +  |   {  |   }  |  |   |
+ * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   _  |   +  |   {  |   }  |  \   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | Shift|  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |   -  |   =  |   [  |   ]  |Enter |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | _NUM | Ctrl |  Alt | GUI  |_Lower|   SpaceFN   |_Raise| Home | PgUP | PgDN |  End |
  * `-----------------------------------------------------------------------------------'
  */
-[_RAISE] = LAYOUT_planck_grid(
+[_LOWER] = LAYOUT_planck_grid(
     KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,  KC_ASTR,  KC_LPRN, KC_RPRN, _______,
-    KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS,  KC_PLUS,  KC_LCBR, KC_RCBR, KC_PIPE,
+    KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS,  KC_PLUS,  KC_LCBR, KC_RCBR, KC_BSLS,
     _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_MINS,  KC_EQL,   KC_LBRC, KC_RBRC, _______,
     NUM,     KC_LCTL, KC_LALT, KC_LGUI, _______, _______, _______, _______,  KC_HOME,  KC_PGDN, KC_PGUP, KC_END
 ),
 
-/* Lower
+
+/* Raise
  * ,-----------------------------------------------------------------------------------.
  * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Bksp |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | CAPS |      |SNAKE |      |      |APTUP |      | CLONE|  ADD |COMMIT| PUSH |  \   |
+ * | CAPS |      |SNAKE |      |      |APTUP |      | CLONE|  ADD |COMMIT| PUSH |  |   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | Shift|  <>  |  {}  |  ()  |  []  |      |      |CHKOUT|STATUS|BRANCH| PULL |Enter |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | _NUM | Ctrl |  Alt | GUI  |_Lower|   SpaceFN   |_Raise| Next | Vol- | Vol+ | Play |
  * `-----------------------------------------------------------------------------------'
  */
-[_LOWER] = LAYOUT_planck_grid(
+[_RAISE] = LAYOUT_planck_grid(
     KC_GRV,   KC_1,    KC_2,      KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
-    CAPSWORD, XXXXXXX, SNAKECASE, XXXXXXX, XXXXXXX, APTUP,   XXXXXXX, GITCLN,  GITADD,  GITCOMM, GITPUSH, KC_BSLS,
+    CAPSWORD, XXXXXXX, SNAKECASE, XXXXXXX, XXXXXXX, APTUP,   XXXXXXX, GITCLN,  GITADD,  GITCOMM, GITPUSH, KC_PIPE,
     _______,  ANGLE,   CURLY,     PARAN,   BRACK,   XXXXXXX, XXXXXXX, GITCHK,  GITSTAT, GITBRCH, GITPULL, _______,
     NUM,      KC_LCTL, KC_LALT,   KC_LGUI, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
 ),
--- keyboards/preonic/keymaps/elvis/keymap.c
+++ keyboards/preonic/keymaps/elvis/keymap.c
@@ -177,9 +177,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |-----------------------------------------------------------------------------------|
  * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Bksp |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | CAPS |      |SNAKE |      |      |APTUP |      |      |  ADD |COMMIT| PUSH |  |   |
+ * | CAPS |      |SNAKE |      |      |APTUP |      | CLONE|  ADD |COMMIT| PUSH |  |   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |  <>  |  {}  |  ()  |  []  |      |      |      |STATUS|BRANCH| PULL |Enter |
+ * | Shift|  <>  |  {}  |  ()  |  []  |      |      |CHKOUT|STATUS|BRANCH| PULL |Enter |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | _NUM | Ctrl |  Alt | GUI  |_Lower|    Space    |_Raise| Next | Vol- | Vol+ | Play |
  * `-----------------------------------------------------------------------------------'
@@ -187,8 +187,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_RAISE] = LAYOUT_preonic_grid(
     KC_F1,    KC_F2,   KC_F3,     KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
     KC_GRV,   KC_1,    KC_2,      KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
-    CAPSWORD, XXXXXXX, SNAKECASE, XXXXXXX, XXXXXXX, APTUP,   XXXXXXX, XXXXXXX, GITADD,  GITCOMM, GITPUSH, KC_PIPE,
-    _______,  ANGLE,   CURLY,     PARAN,   BRACK,   XXXXXXX, XXXXXXX, XXXXXXX, GITSTAT, GITBRCH, GITPULL, _______,
+    CAPSWORD, XXXXXXX, SNAKECASE, XXXXXXX, XXXXXXX, APTUP,   XXXXXXX, GITCLN,  GITADD,  GITCOMM, GITPUSH, KC_PIPE,
+    _______,  ANGLE,   CURLY,     PARAN,   BRACK,   XXXXXXX, XXXXXXX, GITCHK,  GITSTAT, GITBRCH, GITPULL, _______,
     NUM,      KC_LCTL, KC_LALT,   KC_LGUI, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
 ),
 
