862be97f45fc3a1d26d0cad8599b7b56758c1db6
aluminum-ice <you@example.com>
2024-12-04 10:34:36 -0800

    added the TICK function

--- keyboards/preonic/keymaps/elvis/keymap.c
+++ keyboards/preonic/keymaps/elvis/keymap.c
@@ -64,6 +64,7 @@ enum preonic_keycodes {
   APTUP,   // apt-get update && apt-get dist-upgrade
   ANGLE,
   BRACK,
+  TICK,
   CURLY,
   PARAN,
   SELECT,
@@ -179,7 +180,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | CAPS |      |SNAKE |      |      |APTUP |      | CLONE|  ADD |COMMIT| PUSH |  |   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Shift|  <>  |  {}  |  ()  |  []  |      |      |CHKOUT|STATUS|BRANCH| PULL |Enter |
+ * | Shift|  <>  |  {}  |  ()  |  []  |  ``  |      |CHKOUT|STATUS|BRANCH| PULL |Enter |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | _NUM | Ctrl |  Alt | GUI  |_Lower|    Space    |_Raise| Next | Vol- | Vol+ | Play |
  * `-----------------------------------------------------------------------------------'
@@ -188,7 +189,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_F1,    KC_F2,   KC_F3,     KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
     KC_GRV,   KC_1,    KC_2,      KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
     CAPSWORD, XXXXXXX, SNAKECASE, XXXXXXX, XXXXXXX, APTUP,   XXXXXXX, GITCLN,  GITADD,  GITCOMM, GITPUSH, KC_PIPE,
-    _______,  ANGLE,   CURLY,     PARAN,   BRACK,   XXXXXXX, XXXXXXX, GITCHK,  GITSTAT, GITBRCH, GITPULL, _______,
+    _______,  ANGLE,   CURLY,     PARAN,   BRACK,   TICK,    XXXXXXX, GITCHK,  GITSTAT, GITBRCH, GITPULL, _______,
     NUM,      KC_LCTL, KC_LALT,   KC_LGUI, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
 ),
 
@@ -455,6 +456,15 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
      case CURLY:
       if (record->event.pressed) {
           SEND_STRING("{}");
+          tap_code(KC_LEFT);  // Move cursor between quotes
+      } else {
+        // When keycode is released
+      }
+      break;
+
+     case TICK:
+      if (record->event.pressed) {
+          SEND_STRING("``");
           tap_code(KC_LEFT);  // Move cursor between quotes
       } else {
         // When keycode is released
